f6a209295ae279b2cdfd24e762b1be2afd8d427c
thumbstackrepo <arifkhanak1995@gmail.com>
2020-01-02 20:24:28 +0530

    Add project files.

---
+++ ConsoleApp1/Member.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    public class Member
+    {
+        /*
+         * { name: "ANWAR HUSSAIN", designation: "LED LIGHTINGS",expertise: "Samara Lighting Solutions",
+    profilePic: "/assets/img/profile-pic/AASTIK-AJIT-SHAH.png",
+    email: "aastik.shah@gmail.com",mobile: "97733 54832",desc:"LICENCED FIRE FIGHTING CONTRACTORS"
+  },
+  */
+        public string name { get; set; }
+        public string designation { get; set; }
+        public string expertise { get; set; }
+        public string profilePic { get; set; }
+        public string email { get; set; }
+        public string mobile { get; set; }
+        public string desc { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllLines(@"C:\Users\Ritesh\Desktop\New folder (2)\A.txt");
+            var data = text.Where(t=>!String.IsNullOrWhiteSpace(t)).ToList();
+            //Console.WriteLine(String.Join("\n",data));
+            var members = new List<Member>();
+
+            var blockCount = 0;
+            var blockSize = 8;
+            var member = new Member();
+            foreach (var line in data)
+            {
+               
+                
+                switch (blockCount)
+                {
+                    case 1:
+                        member.name = line;
+                        break;
+                    case 2:
+                        member.expertise = line;
+                        break;
+                    case 3:
+                        member.designation = line;
+                        break;
+                    case 6:
+                        member.mobile = line.Replace("T:", "").Trim(); 
+                        break;
+                    case 7:
+                        member.email = line.Replace("E:","").Trim();
+                        break;
+                    default:
+                        break;
+                }
+
+                blockCount++;
+                if (blockCount >= blockSize)
+                {
+                    member.profilePic = $"/assets/img/profile-pic/{member.name.Trim().Replace(" ","-")}.jpg";
+                    members.Add(
+                    member
+                    );
+                    member = new Member();
+                    blockCount = 0;
+                } 
+            }
+            var json = JsonConvert.SerializeObject(members,Formatting.Indented);
+            File.WriteAllText(@"C:\Users\Ritesh\Desktop\New folder (2)\A.json", json.ToString());
+            //Console.WriteLine(json);
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
